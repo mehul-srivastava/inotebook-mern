826714dfd6d52de09c366d67fbf6bba041c6cae5
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2023-05-02 12:56:49 +0530

    Input Validations using express-validator

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const port = 3000;
 const AuthRoutes = require("./routes/auth");
 const NotesRoutes = require("./routes/notes");
 
-// App Configuration
-app.use(express.json());
-
 // Database Connection
 connectToDB();
 
-// Routes
+// App Configuration
+app.use(express.json());
+
+// App Routes
 app.use("/api/auth", AuthRoutes);
 app.use("/api/notes", NotesRoutes);
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,41 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const User = require("../models/User");
 
-router.post("/", (req, res) => {
-  let user = User(req.body); //check this online and fix
-  user.save();
-  res.send(req.body);
+// Add User Route
+const addUserValidation = [
+  body("name", "Name can only be between 3 and 50 characters!").isLength({
+    min: 3,
+    max: 50,
+  }),
+  body("email", "Email used is not in proper format!").isEmail(),
+  body("password", "Password has to be greater than 5 characters!").isLength({
+    min: 5,
+  }),
+];
+
+router.post("/", addUserValidation, (req, res) => {
+  const error = handleValidationErrors(req, res);
+  if (Boolean(error)) return res.status(400).json(error);
+
+  let user = new User(req.body);
+  user
+    .save()
+    .then(() => res.status(200).json({ user: "user added to db!" }))
+    .catch((error) =>
+      res
+        .status(400)
+        .json({ error: "This email already exists!", message: error.message })
+    );
 });
+
+// Helper Functions
+const handleValidationErrors = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return { errors: errors.array() };
+
+  return false;
+};
 
 module.exports = router;
