f5e139d9943f69257a6f6513638985e9433954d8
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2023-05-13 11:02:17 +0530

    chore: auth endpoints

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,50 +1,97 @@
+require("dotenv").config({ path: "../.env.local" });
 const express = require("express");
-const { body, validationResult } = require("express-validator");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+
 const User = require("../models/User");
 
-//-------- Route: /api/auth/user/create (Create User) --------//
-const addUserValidation = [
-  body("name", "Name can only be between 3 and 50 characters!").isLength({
+const JWT_SECRET = process.env.JWT_SECRET;
+
+/*---------------------- /api/auth/signup/ (Create User)-----------------------*/
+const signupUserValidation = [
+  body("name", "Name should be between 3 to 50 characters long!")
+    .isString()
+    .isLength({ min: 3, max: 50 }),
+  body("email", "Email should be in a valid format!").isEmail(),
+  body("password", "Password should be greater than 3 characters!").isLength({
     min: 3,
-    max: 50,
-  }),
-  body("email", "Email used is not in proper format!").isEmail(),
-  body("password", "Password has to be greater than 5 characters!").isLength({
-    min: 5,
   }),
 ];
-router.post("/", addUserValidation, async (req, res) => {
-  const error = handleValidationErrors(req);
-  if (Boolean(error)) return apiResponse(res, 400, error);
-
+router.post("/signup", signupUserValidation, async (req, res) => {
   try {
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return apiResponse(res, 400, {
-        error: "User with same email already exists!",
-      });
+    // Form Validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
     }
 
-    user = new User(req.body);
-    await user.save();
+    // Check If User Exists
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "User with same email already exists!" });
+    }
 
-    return apiResponse(res, 200, user);
-  } catch (error) {
-    return apiResponse(res, 500, error);
+    // Hash Password
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+    // Create User
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // JWT Auth Token
+    let payload = {
+      id: user.id,
+    };
+    let authToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+    // Success Response
+    res.status(200).json({ authToken });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
   }
 });
 
-// Helper Functions
-const handleValidationErrors = (req) => {
+/*---------------------- /api/auth/login/ (Authenticate User)-----------------------*/
+const loginUserValidation = [
+  body("email", "Email should be in a valid format!").isEmail(),
+  body("password", "Password should not be blank!").exists(),
+];
+router.post("/login", loginUserValidation, async (req, res) => {
+  // Form Validation
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return { errors: errors.array() };
+  if (!errors.isEmpty()) return res.status(400).json(errors);
 
-  return false;
-};
+  // Destructuring Request From Body
+  const { email, password } = req.body;
 
-const apiResponse = (res, code, message) => {
-  return res.status(code).json(message);
-};
+  try {
+    // Check If Email Exists
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json({ error: "Incorrect Credentials!" });
+
+    // Check If Password Matches
+    const passwordCheck = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordCheck)
+      return res.status(400).json({ error: "Incorrect Credentials!" });
+
+    // JWT Auth Token
+    let payload = {
+      id: user.id,
+    };
+    let authToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+    // Success Response
+    res.status(200).json({ authToken });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
 
 module.exports = router;
