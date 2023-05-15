f6109c5297e507f8b8ec29521cc4d7deb513095a
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2023-05-15 18:15:53 +0530

    Backend Completed!

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -50,20 +50,60 @@ router.post("/add", fetchuser, addNoteValidation, async (req, res) => {
   }
 });
 
-/*---------------------- ROUTE #3: Delete /api/notes/ (Delete A Note)-----------------------*/
-router.delete("/", fetchuser, async (req, res) => {
+/*---------------------- ROUTE #3: DELETE /api/notes/:id (Delete A Note)-----------------------*/
+router.delete("/:id", fetchuser, async (req, res) => {
+  let note = await Note.findById(req.params.id);
+
+  // Throw Error If Note Does Not Exist
+  if (!note)
+    return res.status(500).json({ error: "Note With This ID Does Not Exist!" });
+
+  // Throw Error If Note Does Not Belong To Logged In User
+  if (note.user_id.toString() !== req.user_id)
+    return res
+      .status(401)
+      .json({ error: "You Are Not Authorized To Delete This Note!" });
+
+  // Delete The Note
   try {
-    const note = await Note.findByIdAndDelete(req.body.note_id);
-
-    // Throw Error If Note Does Not Exist
-    if (!note)
-      return res
-        .status(500)
-        .json({ error: "Note With This ID Does Not Exist!" });
-
-    return res.status(200).json(note);
+    note = await note.deleteOne();
+    return res.status(200).json({ note });
   } catch (e) {
-    return res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: "Internal Server Error!" });
+  }
+});
+
+/*---------------------- ROUTE #3: PUT /api/notes/:id (Update A Note)-----------------------*/
+router.put("/:id", fetchuser, async (req, res) => {
+  // Destructure Request
+  const { title, description, tags } = req.body;
+
+  // Create An Empty Note Object
+  const newNote = {};
+  if (title) newNote.title = title;
+  if (description) newNote.description = description;
+  if (tags) newNote.tags = tags;
+
+  // Throw Error If Note Does Not Exist
+  let note = await Note.findById(req.params.id);
+  if (!note)
+    return res.status(500).json({ error: "Note With This ID Does Not Exist!" });
+
+  // Throw Error If Note Does Not Belong To Logged In User
+  if (note.user_id.toString() !== req.user_id)
+    return res
+      .status(401)
+      .json({ error: "You Are Not Authorized To Update This Note!" });
+
+  try {
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.status(200).json(note);
+  } catch (e) {
+    res.status(400).json({ error: "Internal Server Error!" });
   }
 });
 
