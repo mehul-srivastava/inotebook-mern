4591be00ef006327bbeac3a1cc3aac718b34fa45
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2023-05-17 17:58:45 +0530

    setup: `react-router-dom` for routing and bootstrap for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Home, About } from "./components";
 
 const App = () => {
-  return <div>hello wor1ld, how are you?!</div>;
+  return (
+    <>
+      <Router>
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <img
+          src="./favicon.ico"
+          alt="logo"
+          height={40}
+          style={{ marginRight: "10px" }}
+        />
+        <Link className="navbar-brand" to="/">
+          iNotebook
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse ms-2"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">
+                {/* use className `active` when on that page */}
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
