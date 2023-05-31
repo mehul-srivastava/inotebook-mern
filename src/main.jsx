7d7366473d167d694fa0471862c5deebc7f1fc41
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2023-05-31 13:17:20 +0530

    fix: minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { AuthProvider } from "./contexts/AuthContext";
+
 import App from "./App";
 
 import "./index.css";
@@ -9,7 +11,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
